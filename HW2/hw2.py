012823550f0c293a48c5ff961d075b5542015af1
mauposad <mposad9061@gmail.com>
2024-10-28 17:25:35 -0600

    Set up complete for HW2

---
+++ HW2/hw2.py
@@ -0,0 +1,152 @@
+import cv2
+import numpy as np
+
+
+def sign_lines(img: np.ndarray) -> np.ndarray:
+    """
+    This function takes in the image as a numpy array and returns a numpy array of lines.
+
+    https://docs.opencv.org/3.4/d9/db0/tutorial_hough_lines.html
+    :param img: Image as numpy array
+    :return: Numpy array of lines.
+    """
+    raise NotImplemented
+
+
+def sign_circle(img: np.ndarray) -> np.ndarray:
+    """
+    This function takes in the image as a numpy array and returns a numpy array of circles.
+    :param img: Image as numpy array
+    :return: Numpy array of circles.
+    """
+    raise NotImplemented
+
+
+def sign_axis(lines: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    This function takes in a numpy array of lines and returns a tuple of np.ndarray and np.ndarray.
+
+    This function should identify the lines that make up a sign and split the x and y coordinates.
+    :param lines: Numpy array of lines.
+    :return: Tuple of np.ndarray and np.ndarray with each np.ndarray consisting of the x coordinates and y coordinates
+             respectively.
+    """
+    xaxis = np.empty(0, dtype=np.int32)
+    yaxis = np.empty(0, dtype=np.int32)
+    return xaxis, yaxis
+
+
+def identify_traffic_light(img: np.ndarray) -> tuple:
+    """
+    This function takes in the image as a numpy array and returns a tuple identifying the location
+    of the traffic light in the image and the lighted light.
+    :param img: Image as numpy array
+    :return: Tuple identifying the location of the traffic light in the image and light.
+             ( x,   y, color)
+             (140, 100, 'None') or (140, 100, 'Red')
+             In the case of no light lit, coordinates can be just center of traffic light
+    """
+    raise NotImplemented
+
+
+def identify_stop_sign(img: np.ndarray) -> tuple:
+    """
+    This function takes in the image as a numpy array and returns a tuple of the sign location and name.
+    :param img: Image as numpy array
+    :return: tuple with x, y, and sign name
+             (x, y, 'stop')
+    """
+    raise NotImplemented
+
+
+def identify_yield(img: np.ndarray) -> tuple:
+    """
+    This function takes in the image as a numpy array and returns a tuple of the sign location and name.
+    :param img: Image as numpy array
+    :return: tuple with x, y, and sign name
+             (x, y, 'yield')
+    """
+    raise NotImplemented
+
+
+def identify_construction(img: np.ndarray) -> tuple:
+    """
+    This function takes in the image as a numpy array and returns a tuple of the sign location and name.
+    :param img: Image as numpy array
+    :return: tuple with x, y, and sign name
+             (x, y, 'construction')
+    """
+    raise NotImplemented
+
+
+def identify_warning(img: np.ndarray) -> tuple:
+    """
+    This function takes in the image as a numpy array and returns a tuple of the sign location and name.
+    :param img: Image as numpy array
+    :return: tuple with x, y, and sign name
+             (x, y, 'warning')
+    """
+    raise NotImplemented
+
+
+def identify_rr_crossing(img: np.ndarray) -> tuple:
+    """
+    This function takes in the image as a numpy array and returns a tuple of the sign location and name.
+    :param img: Image as numpy array
+    :return: tuple with x, y, and sign name
+             (x, y, 'rr_crossing')
+    """
+    raise NotImplemented
+
+
+def identify_services(img: np.ndarray) -> tuple:
+    """
+    This function takes in the image as a numpy array and returns a tuple of the sign location and name.
+    :param img: Image as numpy array
+    :return: tuple with x, y, and sign name
+             (x, y, 'services')
+    """
+    raise NotImplemented
+
+
+def identify_signs(img: np.ndarray) -> np.ndarray:
+    """
+    This function takes in the image as a numpy array and returns a numpy array of all signs locations and name.
+    Call the other identify functions to determine where that sign is if it exists.
+    :param img: Image as numpy array
+    :return: Numpy array of all signs locations and name.
+             [[x, y, 'stop'],
+              [x, y, 'construction']]
+    """
+    raise NotImplemented
+
+
+def identify_signs_noisy(img: np.ndarray) -> np.ndarray:
+    """
+    This function takes in the image as a numpy array and returns a numpy array of all signs locations and name.
+    Call the other identify functions to determine where that sign is if it exists.
+
+    The images will have gaussian noise applied to them so you will need to do some blurring before detection.
+    :param img: Image as numpy array
+    :return: Numpy array of all signs locations and name.
+             [[x, y, 'stop'],
+              [x, y, 'construction']]
+    """
+    raise NotImplemented
+
+
+def identify_signs_real(img: np.ndarray) -> np.ndarray:
+    """
+    This function takes in the image as a numpy array and returns a numpy array of all signs locations and name.
+    Call the other identify functions to determine where that sign is if it exists.
+
+    The images will be real images so you will need to do some preprocessing before detection.
+    You may also need to adjust existing functions to detect better with real images through named parameters
+    and other code paths
+
+    :param img: Image as numpy array
+    :return: Numpy array of all signs locations and name.
+             [[x, y, 'stop'],
+              [x, y, 'construction']]
+    """
+    raise NotImplemented
---
+++ HW2/wrapper.py
@@ -0,0 +1,142 @@
+import os
+import cv2
+from hw2 import *
+
+
+"""
+This assignment will have you detect various traffic signs and traffic lights.
+When trying to find a sign, it's helpful to mask the sign based on color range in HSV.
+OpenCV uses the following ranges for HSV, H: 0-179, S: 0-255, V: 0-255
+Photoshop uses the followwing ranges: H: 0-360, S: 0-100, V: 0-100
+
+So when grabbing the values from other programs, keep in mind how they represent the values and adjust accordingly
+
+If you're looking at the pixel values from the numpy array, remember that the x and y are reversed when you slice it
+
+A typical process would be to convert the image to HSV, identify the pixels that contain the sign in question,
+create a low and high bounds for the color in HSV, then use the inRange on the HSV image to get the mask.
+Then do the normal edge detection, HoughLinesP/HoughCircles.
+"""
+
+
+def main() -> None:
+    part1()
+    part2()
+    part3()
+    part4()
+    part5()
+
+
+def part1() -> None:
+    """
+    Part 1 is identifying a traffic light along with which one is lit
+    :return: None
+    """
+    tl_images = ['images/tl_all_bright_default.jpg',
+                 'images/tl_red_bright_default.jpg', 'images/tl_green_bright_default.jpg',
+                 'images/tl_yellow_bright_default.jpg', 'images/background_img_tl_red_bright.jpg',
+                 'images/background_img_tl_red_bright2.jpg', 'images/background_img_tl_green_bright.jpg',
+                 'images/background_img_tl_yellow_bright.jpg']
+
+    for tl in tl_images:
+        tl_image = cv2.imread(tl)
+        x, y, color = identify_traffic_light(tl_image)
+        img_out = mark_signs(tl_image, (x, y), color)
+        cv2.imwrite(f"{tl.split('/')[1].split('.')[0]}_txt.jpg", img_out)
+
+
+def part2() -> None:
+    sign_images = ['images/construction.jpg', 'images/regulatory.jpg', 'images/regulatory_yield.jpg',
+                   'images/rr_crossing.jpg', 'images/services.jpg', 'images/warning.jpg']
+
+    file_name = ['construction_txt.jpg', 'regulatory_txt.jpg', 'regulatory_yield_txt.jpg',
+                 'rr_crossing_txt.jpg', 'services_txt.jpg', 'warning_txt.jpg']
+
+    sign_functions = [identify_construction, identify_stop_sign, identify_yield,
+                      identify_rr_crossing, identify_services, identify_warning]
+
+    for img_in, file_out, function_call in zip(sign_images, file_name, sign_functions):
+        img = cv2.imread(img_in)
+        x, y, name = function_call(img)
+
+        img_out = mark_signs(img, (x, y), state=name)
+        cv2.imwrite(file_out, img_out)
+
+
+def part3() -> None:
+    sign_images = ['images/all_signs_blank_background.jpg', 'images/all_signs.jpg',
+                   'images/construction_warning_rr_crossing_background.jpg', 'images/construction_warning_rr_crossing_background02.jpg',
+                   'images/stop_sign_background.jpg', 'images/stop_sign_background02.jpg', 'images/stop_yield_background.jpg',
+                   'images/stop_yield_background02.jpg']
+
+    file_name = ['all_signs_blank_background_txt.jpg', 'all_signs_txt.jpg',
+                 'construction_warning_rr_crossing_background_txt.jpg', 'construction_warning_rr_crossing_background02_txt.jpg',
+                 'stop_sign_background_txt.jpg', 'stop_sign_background02_txt.jpg', 'stop_yield_background_txt.jpg',
+                 'stop_yield_background02_txt.jpg']
+
+    for img_in, file_out in zip(sign_images, file_name):
+        img = cv2.imread(img_in)
+        img_out = np.copy(img)
+        found_signs = identify_signs(img)
+        # print(found_signs)
+        if found_signs:
+            for sign in found_signs:
+                x = sign[0]
+                y = sign[1]
+                name = sign[2]
+                img_out = mark_signs(img_out, (x, y), name)
+            # print("writing file")
+            cv2.imwrite(file_out, img_out)
+
+
+def part4() -> None:
+    # print("in part 4")
+    sign_images = ['images/all_signs_blank_background_noise.jpg', 'images/all_signs_noise.jpg']
+
+    file_name = ['all_signs_blank_background_noise_txt.jpg', 'all_signs_noise_txt.jpg']
+
+    for img_in, file_out in zip(sign_images, file_name):
+        img = cv2.imread(img_in)
+        img_out = np.copy(img)
+        found_signs = identify_signs_noisy(img)
+        if found_signs:
+            for sign in found_signs:
+                x = sign[0]
+                y = sign[1]
+                name = sign[2]
+                img_out = mark_signs(img_out, (x, y), name)
+
+            cv2.imwrite(file_out, img_out)
+
+
+def part5() -> None:
+    sign_images = ['images/real_signs01.jpg', 'images/real_signs02.jpg', 'images/real_signs03.jpg']
+
+    file_name = ['real_signs01_txt.jpg', 'real_signs02_txt.jpg', 'real_signs03_txt.jpg']
+
+    for img_in, file_out in zip(sign_images, file_name):
+        img = cv2.imread(img_in)
+        img_out = np.copy(img)
+        found_signs = identify_signs_real(img)
+        if found_signs:
+            for sign in found_signs:
+                x = sign[0]
+                y = sign[1]
+                name = sign[2]
+                img_out = mark_signs(img_out, (x, y), name)
+
+            cv2.imwrite(file_out, img_out)
+
+
+def mark_signs(image, coords, state='') -> np.ndarray:
+    img = cv2.putText(image, f"x: {coords[0]}, y: {coords[1]}", (coords[0]+5, coords[1]), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 3)
+    img = cv2.putText(img, f"x: {coords[0]}, y: {coords[1]}", (coords[0]+5, coords[1]), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1)
+    cv2.circle(img, (coords[0], coords[1]), 2, (0, 0, 0), -1)
+    if state != '':
+        img = cv2.putText(img, f"{state}", (coords[0]+5, coords[1]+15), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 3)
+        img = cv2.putText(img, f"{state}", (coords[0]+5, coords[1]+15), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1)
+    return img
+
+
+if __name__ == '__main__':
+    main()
